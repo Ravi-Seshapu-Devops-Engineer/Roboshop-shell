9eefe04b27e510438ac8da8953ca2080de1448ee
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-02 12:07:09 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ DOMAIN_NAME=seshapudevops.online
 
 for instance in $@
 do
-  INSTANCE_ID=(aws ec2 run-instances \
+  INSTANCE_ID=$(aws ec2 run-instances \
   --image-id $AMI_ID \
   --instance-type t3.micro \
   --security-group-ids $SG_ID \
