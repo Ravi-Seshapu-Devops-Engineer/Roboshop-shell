1789e90d9bb679b52687d4efdca601cb71a24d78
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-02 17:42:38 +0530

    actions

--- cart.sh
+++ cart.sh
@@ -73,21 +73,7 @@ validate $? "enabling cart"
 systemctl start cart &>>$LOGS_FILE
 validate $? "cart started"
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y &>>$LOGS_FILE
-
-INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("cart")')
-
-if [ $INDEX -le 0 ]; then
-  mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
-  validate $? "Loading products"
-else
-  echo -e "Products already loaded $Y skipping $N"
-fi
-
-systemctl restart cart
-validate $? "cart restart"
-
 systemctl status cart
 
 netstat -lntp
+
